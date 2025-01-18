15e64785a9185d89cdfafc5d836f415039205043
Muhammad Shammas <tortoizian@gmail.com>
2025-01-18 21:49:20 +0530

    app created. name : 'bookings'

--- bookings/models.py
+++ bookings/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from flights.models import Flight
 
-# Create your models here.
+class Booking(models.Model):
+    flight_code = models.ForeignKey(Flight, on_delete=models.CASCADE)
+    date = models.DateField()
+    list_of_passengers = models.JSONField(default=list) #can add pnr, and other data like age, name etc from front end?
+    
+    def __str__(self):
+        return "Bookings of" + self.pnr
