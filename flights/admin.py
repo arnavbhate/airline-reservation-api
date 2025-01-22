0a63b5c27773107352aaf130aca06ff21373ed75
Aryan10022006 <aryanst800@gmail.com>
2025-01-22 19:02:18 +0530

    Flight model update 1

--- flights/admin.py
+++ flights/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
 # Register your models here.
+class FlightAdmin(admin.ModelAdmin):
+    list_display = ('flight_id', 'departure_airpor', 'arrival_arrival', 'departure_time', 'arrival_time', 'economy_seat','business_seat','first_class_seat')
--- flights_model/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-class FlightAdmin(admin.ModelAdmin):
-    list_display = ('flight_id', 'departure_airpor', 'arrival_arrival', 'departure_time', 'arrival_time', 'economy_seat','business_seat','first_class_seat')
