5f4e0e2db05effa7a0eeaf151176f0f1282b0bbd
Arnav Bhate <bhatearnav@gmail.com>
2025-02-08 22:51:36 +0530

    Consistent field name

--- bookings/models.py
+++ bookings/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from flights.models import Flight
 
+
 class Booking(models.Model):
 
     class BookingClasses(models.TextChoices):
-        EconomyClass = 'E', 'Economy Class'
-        FirstClass = 'F', 'First Class'
-        BusinessClass = 'B', 'Business Class'
+        EconomyClass = "E", "Economy Class"
+        FirstClass = "F", "First Class"
+        BusinessClass = "B", "Business Class"
 
     booking_class = models.CharField(
         max_length=1,
@@ -15,10 +16,12 @@ class Booking(models.Model):
     )
 
     pnr = models.CharField(max_length=6, primary_key=True)
-    flight_code = models.ForeignKey(Flight, on_delete=models.CASCADE)
+    flight_no = models.ForeignKey(Flight, on_delete=models.CASCADE)
     date = models.DateField()
-    list_of_passengers = models.JSONField(default=list) #can add data like age, name etc from front end
-    seats_booked = models.IntegerField(default=0) #to count the no. of seats booked
-    
+    list_of_passengers = models.JSONField(
+        default=list
+    )  # can add data like age, name etc from front end
+    seats_booked = models.IntegerField(default=0)  # to count the no. of seats booked
+
     def __str__(self):
         return "Booking of" + self.pnr
